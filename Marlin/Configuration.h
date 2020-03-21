bd5e820dabdc216501f00f369f3b30bdd6c9f8e1
ranseyer <martin.neubauer@gmx.de>
2020-03-21 23:25:37 +0100

    part2

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1480,8 +1480,8 @@
 
 // Preheat Constants
 #define PREHEAT_1_LABEL       "PLA"
-#define PREHEAT_1_TEMP_HOTEND 175
-#define PREHEAT_1_TEMP_BED     60
+#define PREHEAT_1_TEMP_HOTEND 180
+#define PREHEAT_1_TEMP_BED     70
 #define PREHEAT_1_FAN_SPEED     0 // Value from 0 to 255
 
 #define PREHEAT_2_LABEL       "ABS"
